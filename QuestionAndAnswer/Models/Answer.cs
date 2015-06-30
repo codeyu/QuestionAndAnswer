8f3ba523f0676a81d8c8d6f58e522f7efc5d56e0
wb.chen <wb.chen@outlook.com>
2015-06-30 21:50:31 +0800

    添加问答相关Model类

---
+++ QuestionAndAnswer/Models/Answer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuestionAndAnswer.Models
+{
+    public class Answer
+    {
+        // 主键答案Id
+        public virtual int AnswerId { get; set; }
+
+        // 外键，答案对应的问题的ID
+        public virtual int QuestionId { get; set; }
+
+        // 答案内容
+        public virtual string AnswerContent { get; set; }
+
+        // 问题回答时间
+        public virtual DateTime AnswerTime { get; set; }
+
+        // 导航属性，关联相关问题
+        public virtual Question Question { get; set; }
+    }
+}
---
+++ QuestionAndAnswer/Models/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuestionAndAnswer.Models
+{
+    public class Question
+    {
+        public virtual int QuestionId { get; set; }
+
+        // 问题对应的创建人
+        public virtual string QuestionCreator { get; set; }
+
+        // 问题内容
+        public virtual string QuestionContent { get; set; }
+
+        // 问题创建时间
+        public virtual DateTime QuestionCreateTime { get; set; }
+
+        // 相关回答
+        public virtual List<Answer> Answers { get; set; }
+    }
+}
